002ce80337eeff72239c81698ad44af3dfc1cef1
iamist <vicentechristian@gmail.com>
2015-04-21 10:54:07 +0800

    initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jotrails::Application.config.secret_token = '1bcff4685f0d8eee0cace105201894411d68d5f551f86763099c1b3bef72574ab181e238b9ae5b07f9db326e6f92ea625be5707adbae4afa6bc65d563f5ca8fe'
